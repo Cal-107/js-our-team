1c8cd89af16c4690e6d2d38ef1cf86304160ae22
“Cal-107” <kaly___7@hotmail.it>
2021-11-04 14:45:20 +0100

    add array with objects and create loop for

--- js/script.js
+++ js/script.js
@@ -5,3 +5,57 @@
 // Inserire a inizio del file JavaScript una sezione di commento dove scrivere gli step richiesti di logica in italiano, per aiutarvi a ragionare prima della stesura del codice.
 
 console.log('JS is ok');
+
+// 1 - Creazione dell'array con gli oggetti
+// 2 - Stampa delle immagini
+// 3 - Inserire input per aggiungere
+
+// Creazione del'array
+
+const teamList = [
+    {
+        img: 'img/wayne-barnett-founder-ceo.jpg',
+        name: 'Wayne Barret',
+        role: 'Founder & CEO',
+    },
+    {
+        img: 'img/wayne-barnett-founder-ceo.jpg',
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+    },
+    {
+        img: 'img/wayne-barnett-founder-ceo.jpg',
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+    },
+    {
+        img: 'img/wayne-barnett-founder-ceo.jpg',
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+    },
+    {
+        img: 'img/wayne-barnett-founder-ceo.jpg',
+        name: 'Scott Estrada',
+        role: 'Developer'
+    },
+    {
+        img: 'img/wayne-barnett-founder-ceo.jpg',
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+    },
+]
+console.log(teamList);
+console.table(teamList);
+
+// Creo il ciclo per stampare i membri del team
+
+// Mi collego all'elemento alla classe dove far uscire la stampa sul DOM
+const teamCont = document.querySelector('team-container');
+
+// Ciclo for
+for (let i = 0; i < teamList.length; i++) {
+    console.log(teamList);
+    // Prendo gli oggetti dell'array e faccio una variabile
+    const teamCard = teamList[i];
+    console.log('Oggetti array', teamCard);
+}
